be5593a2f31855a99df080ab7f9b99feefb7d976
BB20101997 <bennet-blessmann@t-online.de>
2017-12-09 22:36:19 +0100

    fixed auto-format changing the order of execution by assigning the values to the public static final fields at the end of the static Block

--- src/main/java/de/webtwob/mma/core/common/registration/world/recipes/DefaultRecipes.java
+++ src/main/java/de/webtwob/mma/core/common/registration/world/recipes/DefaultRecipes.java
@@ -1,6 +1,7 @@
 package de.webtwob.mma.core.common.registration.world.recipes;
 
 import com.google.common.base.Predicate;
+
 import de.webtwob.mma.api.crafting.BasicInWorldRecipe;
 import de.webtwob.mma.api.util.MMAFilter;
 import de.webtwob.mma.core.common.registration.Blocks;
@@ -16,24 +17,37 @@ import java.util.Map;
  */
 public class DefaultRecipes {
     
-    private static final String[][] CRAFTING_STORAGE_PATTERN = {{"BBB", "BBB", "BBB"}, {"BCB", "BWB", "BCB"}, {"BBB", "BBB", "BBB"}};
-    private static final String[][] CRAFTING_PROCESSOR_PATTERN = {{"BBB", "BBB", "BBB"}, {"BAB", "BFB", "BWB"}, {"BBB", "BBB", "BBB"}};
+    public static final BasicInWorldRecipe CRAFTING_PROCESSOR_RECIPE;
+    public static final BasicInWorldRecipe CRAFTING_STORAGE_RECIPE;
+    private static final String[][]                                 CRAFTING_STORAGE_PATTERN = {
+            {"BBB", "BBB", "BBB"}, {"BCB", "BWB", "BCB"}, {"BBB", "BBB", "BBB"}
+    };
+    private static final String[][]                                 CRAFTING_PROCESSOR_PATTERN = {
+            {"BBB", "BBB", "BBB"}, {"BAB", "BFB", "BWB"}, {"BBB", "BBB", "BBB"}
+    };
     private static final Map<Character, Predicate<BlockWorldState>> RECIPE_PREDICATES = new HashMap<>();
-    private static final Map<BlockPos, Object> CRAFTING_STORAGE_RESULT = new HashMap<>();
-    private static final Map<BlockPos, Object> CRAFTING_PROCESSOR_RESULT = new HashMap<>();
+    private static final Map<BlockPos, Object>                      CRAFTING_STORAGE_RESULT = new HashMap<>();
+    private static final Map<BlockPos, Object>                      CRAFTING_PROCESSOR_RESULT = new HashMap<>();
     
     static {
-        RECIPE_PREDICATES.put('A', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.ANVIL));
-        RECIPE_PREDICATES.put('B', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.IRON_BARS));
-        RECIPE_PREDICATES.put('C', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.CHEST));
-        RECIPE_PREDICATES.put('F', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.FURNACE));
-        RECIPE_PREDICATES.put('W', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.CRAFTING_TABLE));
+        RECIPE_PREDICATES.put(
+                'A', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.ANVIL));
+        RECIPE_PREDICATES.put(
+                'B', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.IRON_BARS));
+        RECIPE_PREDICATES.put(
+                'C', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.CHEST));
+        RECIPE_PREDICATES.put(
+                'F', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.FURNACE));
+        RECIPE_PREDICATES.put(
+                'W', (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(net.minecraft.init.Blocks.CRAFTING_TABLE));
         
         CRAFTING_STORAGE_RESULT.put(new BlockPos(1, 1, 1), Blocks.CRAFTING_STORAGE);
         CRAFTING_PROCESSOR_RESULT.put(new BlockPos(1, 1, 1), Blocks.CRAFTING_PROCESSOR);
+        
+        CRAFTING_PROCESSOR_RECIPE = new BasicInWorldRecipe(
+                CRAFTING_PROCESSOR_PATTERN, RECIPE_PREDICATES, CRAFTING_PROCESSOR_RESULT);
+        CRAFTING_STORAGE_RECIPE = new BasicInWorldRecipe(
+                CRAFTING_STORAGE_PATTERN, RECIPE_PREDICATES, CRAFTING_STORAGE_RESULT);
     }
-    
-    public static final BasicInWorldRecipe CRAFTING_PROCESSOR_RECIPE = new BasicInWorldRecipe(CRAFTING_PROCESSOR_PATTERN, RECIPE_PREDICATES, CRAFTING_PROCESSOR_RESULT);
-    public static final BasicInWorldRecipe CRAFTING_STORAGE_RECIPE = new BasicInWorldRecipe(CRAFTING_STORAGE_PATTERN, RECIPE_PREDICATES, CRAFTING_STORAGE_RESULT);
     
 }
