59bbea5fdd9528c509f291bca5a8172912001f22
BB20101997 <bennet-blessmann@t-online.de>
2017-12-12 13:14:09 +0100

    Recipes can now be loaded from Datapacks, requires World reload and when switching worlds may require Game restart if BuiltIn recipes have been overriden

--- src/api/java/de/webtwob/mma/api/util/InWorldRecipeLoader.java
+++ src/api/java/de/webtwob/mma/api/util/InWorldRecipeLoader.java
@@ -2,11 +2,12 @@ package de.webtwob.mma.api.util;
 
 import com.google.common.base.Predicate;
 import com.google.gson.*;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import de.webtwob.mma.api.APILog;
 import de.webtwob.mma.api.crafting.BasicInWorldRecipe;
 import de.webtwob.mma.api.registries.InWorldRecipe;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.BlockWorldState;
@@ -17,6 +18,7 @@ import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.registries.ForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import javax.annotation.Nonnull;
@@ -39,27 +41,30 @@ public class InWorldRecipeLoader {
     
     private static File location;
     
+    private InWorldRecipeLoader(){}
+    
     private static void loadCustomRecipes(IForgeRegistry<InWorldRecipe> registry) {
-        if (location != null) {
+        if (location != null && (location.mkdirs() || location.exists())) {
             //noinspection ResultOfMethodCallIgnored
-            if (location.mkdirs() || location.exists())
-                for (File file : FileUtils.listFiles(location, new String[]{"3drecipe"}, true)) {
-                    
-                    String s = FilenameUtils.removeExtension(location.toURI().relativize(file.toURI()).toString());
-                    String[] domainSplit = s.split("/", 2);
-                    
-                    if (domainSplit.length == 2) {
-                        ResourceLocation recipeRL = new ResourceLocation(domainSplit[0], domainSplit[1]);
-                        try {
-                            InWorldRecipe recipe = loadRecipeFromFile(file);
-                            recipe.setRegistryName(recipeRL);
-                            registry.register(recipe);
-                        } catch (IOException exception) {
-                            APILog.LOGGER.error("Couldn't read custom InWorldRecipe " + recipeRL + " from " + file, exception);
-                        }
+            for (File file : FileUtils.listFiles(location, new String[]{"3drecipe"}, true)) {
+                
+                String   s           = FilenameUtils.removeExtension(
+                        location.toURI().relativize(file.toURI()).toString());
+                String[] domainSplit = s.split("/", 2);
+                
+                if (domainSplit.length == 2) {
+                    ResourceLocation recipeRL = new ResourceLocation(domainSplit[0], domainSplit[1]);
+                    try{
+                        InWorldRecipe recipe = loadRecipeFromFile(file);
+                        recipe.setRegistryName(recipeRL);
+                        registry.register(recipe);
+                    } catch(IOException exception){
+                        APILog.LOGGER.error(
+                                "Couldn't read custom InWorldRecipe " + recipeRL + " from " + file, exception);
                     }
-                    
                 }
+                
+            }
         }
     }
     
@@ -69,41 +74,52 @@ public class InWorldRecipeLoader {
     
     @SubscribeEvent
     public static void onWorldLoad(WorldEvent.Load loadEvent) {
-        InWorldRecipeLoader.setLocation(new File(new File(DimensionManager.getCurrentSaveRootDirectory(), "data"), "3d_recipes"));
+        InWorldRecipeLoader.setLocation(
+                new File(new File(DimensionManager.getCurrentSaveRootDirectory(), "data"), "3d_recipes"));
         IForgeRegistry<InWorldRecipe> registry = GameRegistry.findRegistry(InWorldRecipe.class);
         if (registry != null) {
-            //loadCustomRecipes(registry); TODO fix crash when recipes are loaded after freeze
+            boolean relock = false;
+            if(registry instanceof ForgeRegistry){
+                //TODO find a way that doesn't require unfreezing the registry
+                relock = ((ForgeRegistry) registry).isLocked();
+                ((ForgeRegistry) registry).unfreeze();
+            }
+            loadCustomRecipes(registry);
+            if(relock){
+                ((ForgeRegistry)registry).freeze();
+            }
         }
     }
     
     @Nonnull
     public static InWorldRecipe loadRecipeFromFile(File recipeFile) throws FileNotFoundException {
-    return loadRecipeFromReader(newReader(recipeFile,StandardCharsets.UTF_8));
+        return loadRecipeFromReader(newReader(recipeFile, StandardCharsets.UTF_8));
     }
     
-    public static InWorldRecipe loadRecipeFromReader(Reader recipeReader){
+    public static InWorldRecipe loadRecipeFromReader(Reader recipeReader) {
         Gson gson = new GsonBuilder().registerTypeAdapter(InWorldRecipe.class, new InWordRecipeDeserializer()).create();
         return gson.fromJson(recipeReader, InWorldRecipe.class);
-    
+        
     }
     
     private static class InWordRecipeDeserializer implements JsonDeserializer<InWorldRecipe> {
         
         @Override
-        public InWorldRecipe deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-            if(!json.isJsonObject()){
+        public InWorldRecipe deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            if (!json.isJsonObject()) {
                 throw new JsonParseException("Root JsonElement is not an JsonObject");
             }
             JsonObject root = json.getAsJsonObject();
-            if(!root.has("Type")){
+            if (!root.has("Type")) {
                 throw new JsonParseException("Missing Element \"Type\" in Root Object");
             }
             String type = root.get("Type").getAsString();
             switch (type) {
                 case "BlocksOnly": {
-                    try {
+                    try{
                         return deserializeBlocksOnly(root);
-                    } catch (IllegalStateException se) {
+                    } catch(IllegalStateException se){
                         throw new JsonParseException(se);
                     }
                 }
@@ -113,9 +129,12 @@ public class InWorldRecipeLoader {
         }
         
         private BasicInWorldRecipe deserializeBlocksOnly(JsonObject root) {
-            String[][] aisles = deserializeAisles(root.getAsJsonArray("Aisles"));
-            Map<BlockPos, Object> resultMap = deserializeResultMap(root.getAsJsonArray("resultMap"));
-            Map<Character, Predicate<BlockWorldState>> patternPredicates = deserializeBlockPredicates(root.getAsJsonArray("blockMap"));
+            String[][]                                 aisles            = deserializeAisles(
+                    root.getAsJsonArray("Aisles"));
+            Map<BlockPos, Object>                      resultMap         = deserializeResultMap(
+                    root.getAsJsonArray("resultMap"));
+            Map<Character, Predicate<BlockWorldState>> patternPredicates = deserializeBlockPredicates(
+                    root.getAsJsonArray("blockMap"));
             return new BasicInWorldRecipe(aisles, patternPredicates, resultMap);
         }
         
@@ -123,8 +142,8 @@ public class InWorldRecipeLoader {
             Map<Character, Predicate<BlockWorldState>> result = new HashMap<>();
             for (JsonElement element : blockMap) {
                 JsonObject mapElement = element.getAsJsonObject();
-                char key = mapElement.get("key").getAsCharacter();
-                String blockRL = mapElement.get("value").getAsString();
+                char       key        = mapElement.get("key").getAsCharacter();
+                String     blockRL    = mapElement.get("value").getAsString();
                 result.put(key, (Predicate<BlockWorldState>) MMAFilter.areBlocksEqual(Block.getBlockFromName(blockRL)));
             }
             return result;
@@ -133,9 +152,9 @@ public class InWorldRecipeLoader {
         private Map<BlockPos, Object> deserializeResultMap(JsonArray resultMap) {
             Map<BlockPos, Object> result = new HashMap<>();
             for (JsonElement mapElement : resultMap) {
-                JsonObject mapObject = mapElement.getAsJsonObject();
-                Block resultBlock = Block.getBlockFromName(mapObject.get("blockName").getAsString());
-                BlockPos pos = deserializeBlockPos(mapObject.get("pos").getAsJsonArray());
+                JsonObject mapObject   = mapElement.getAsJsonObject();
+                Block      resultBlock = Block.getBlockFromName(mapObject.get("blockName").getAsString());
+                BlockPos   pos         = deserializeBlockPos(mapObject.get("pos").getAsJsonArray());
                 result.put(pos, resultBlock);
             }
             return result;
@@ -149,7 +168,7 @@ public class InWorldRecipeLoader {
             String[][] aisles = new String[jsonAisles.size()][];
             for (int i = 0; i < jsonAisles.size(); i++) {
                 JsonArray jsonAisle = jsonAisles.get(i).getAsJsonArray();
-                String[] aisle = new String[jsonAisle.size()];
+                String[]  aisle     = new String[jsonAisle.size()];
                 for (int j = 0; j < jsonAisle.size(); j++) {
                     aisle[j] = jsonAisle.get(j).getAsString();
                 }
