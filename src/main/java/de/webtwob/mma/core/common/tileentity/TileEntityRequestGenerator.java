8c1184cf3aa89a64c41085758b1ca20b48e921d1
BB20101997 <bb-github@t-online.de>
2017-12-15 21:54:47 +0100

    made sonarlint stop complaining about this field not being static

--- src/main/java/de/webtwob/mma/core/common/tileentity/TileEntityRequestGenerator.java
+++ src/main/java/de/webtwob/mma/core/common/tileentity/TileEntityRequestGenerator.java
@@ -50,7 +50,7 @@ public class TileEntityRequestGenerator extends TileEntity implements ITickable,
     private final NonNullList<ItemStack> westList  = NonNullList.withSize(1, ItemStack.EMPTY);
     
     @SuppressWarnings("unchecked")
-    private final NonNullList<ItemStack>[] dirLists = (NonNullList<ItemStack>[]) (new NonNullList[]{northList, eastList, southList, westList, upList});
+    private final NonNullList<ItemStack>[] dirLists = (NonNullList<ItemStack>[]) (new NonNullList[]{northList, eastList, southList, westList, upList});//NOSONAR
     
     private final ItemStackHandler muster = new FilteredItemHandler(musterList, MMAFilter.MUSTER_FILTER, 1);
     
