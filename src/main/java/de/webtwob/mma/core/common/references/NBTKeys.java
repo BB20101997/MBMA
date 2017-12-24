317190fbadb2aa5aa41dbd7dd92a7aee586a5f58
BB20101997 <bennet-blessmann@t-online.de>
2017-12-24 14:26:50 +0100

    removed unused NBTKeys

--- src/main/java/de/webtwob/mma/core/common/references/NBTKeys.java
+++ src/main/java/de/webtwob/mma/core/common/references/NBTKeys.java
@@ -8,21 +8,8 @@ import static de.webtwob.mma.core.MMACore.MODID;
 @SuppressWarnings("StringConcatenationMissingWhitespace")
 public class NBTKeys {
 
-    //TODO remove unused NBTKeys
-    public static final String QS_ITEM_HANDLER   = MODID + ":QSITEMS";
-    public static final String QS_TOKEN          = MODID + ":QSToken";
-    public static final String QS_STATE          = MODID + ":QSState";
-    public static final String QS_DATA           = MODID + "qsData";
-    public static final String QS_COLLECT_LENGTH = MODID + ":qsCollectLength";
-    public static final String QS_COLLECT_LIST   = MODID + ":qsCollectList";
-
     public static final String TOKEN_GENERATOR_MUSTER   = MODID + ":TokenGeneratorMuster";
     public static final String TOKEN_GENERATOR_COMBINED = MODID + ":TokenGeneratorCombined";
-
-    public static final String RB_PAGES       = MODID + ":page";
-    public static final String RB_PAGE_NAME   = MODID + ":pageName";
-    public static final String RB_PAGE_LINK   = MODID + ":pageLink";
-    public static final String RB_PAGE_RECIPE = MODID + ":pageRecipe";
 
     public static final String CRAFTING_STORAGE_LIST           = MODID + ":cslist";
     public static final String QUEUE_STACKS                    = MODID + ":queueStacks";
